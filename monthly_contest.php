168e31f54dc21871d5e72cd48c888c67f503150c
simplybored <77207659+smplybored@users.noreply.github.com>
2021-01-15 15:38:41 -0300

    Add files via upload

---
+++ monthly_contest.php
@@ -0,0 +1,64 @@
+<?php include('header.php') ?>
+
+<div style="padding: 0px 5px 0px 5px;">
+
+
+<div class="tableSubTitle">Monthly Contest</div>
+
+<table width="790" align="center" cellpadding="0" cellspacing="0" border="0">
+	<tr valign="top">
+		<td style="padding-right: 15px;" width="510">
+		
+		<span class="highlight">What is it?</span>
+		<br><br>FrameBit is proud to present our first monthly video contest. Each month, users may submit videos with the contest topic of the month. Viewers may browse the contest entries by looking on the Discord. At the end of the month, the FrameBit staff will review all the submitted videos and choose the winner.
+		
+		<br><br><span class="highlight">This Month (January 2021)</span>
+		<br><br>This month's contest is based on the funny art of YTP/YTPMV. The topic of the month is <b>YTP or YTPMV</b>.
+		
+		<br><br><span class="highlight">How do I enter?</span>
+		<br><br>Make a YTP or YTPMV and add [FBP] or [FBPMV] on the video title.
+		
+		<br><br><span class="highlight">Who wins?</span>
+		<br><br>The winner will be picked at the end of the month by the FrameBit staff.
+
+		<br><br><span class="highlight">The Prize?</span>
+		<br><br>Fame, fortune, and the envy of all eyes.
+		
+		</td>
+		<td width="280">
+		
+		<table width="280" align="center" cellpadding="0" cellspacing="0" border="0" bgcolor="#FFCC99">
+			<tr>
+				<td><img src="img/box_login_tl.gif" width="5" height="5"></td>
+				<td><img src="img/pixel.gif" width="1" height="5"></td>
+				<td><img src="img/box_login_tr.gif" width="5" height="5"></td>
+			</tr>
+			<tr>
+				<td><img src="img/pixel.gif" width="5" height="1"></td>
+				<td width="270" style="padding: 5px; text-align: center;">
+				<div style="margin-bottom: 10px; font-weight: bold; font-size: 13px;">Monthly Video Contest Schedule</div>
+				
+				January 2020: <a href="#">YTP/YTPMV</a>
+				
+				<br><br><div style="font-size: 11px; padding: 5px;">Have a suggestion for a monthly video contest? Please <a href="contact.php">tell us</a> about it.</div>
+				
+				</td>
+				<td><img src="img/pixel.gif" width="5" height="1"></td>
+			</tr>
+			<tr>
+				<td><img src="img/box_login_bl.gif" width="5" height="5"></td>
+				<td><img src="img/pixel.gif" width="1" height="5"></td>
+				<td><img src="img/box_login_br.gif" width="5" height="5"></td>
+			</tr>
+		</table>
+			
+		</td>
+	</tr>
+</table>
+
+		</div>
+		</td>
+	</tr>
+</table>
+
+<?php include('footer.php') ?>
